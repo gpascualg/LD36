083c691973f6f1d4dccf8c0a4dd6145311be927f
4nc3str4l <murielmaths@gmail.com>
2016-08-27 10:15:12 +0200

    Some Structure

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -14,7 +14,9 @@ import nape.geom.Vec2List;
 import nape.phys.Body;
 import openfl.display.BlendMode;
 import openfl.display.FPS;
+import GameMap;
 using flixel.util.FlxSpriteUtil;
+using GameMap;
 
 /**
  * This was based on a guide from this forum post: http://forums.tigsource.com/index.php?topic=8803.0
@@ -32,35 +34,10 @@ using flixel.util.FlxSpriteUtil;
  */
 class PlayState extends FlxState
 {
-	public static inline var TILE_SIZE:Int = 16;
-	
 	private static inline var SHADOW_COLOR = 0xff2a2963;
 	private static inline var OVERLAY_COLOR = 0xff887fff;
-	
-	/**
-	 * Only contains non-collidabe tiles
-	 */
-	private var background:FlxTilemap;
-	
-	/**
-	 * The layer into which the actual "level" will be drawn, and also the one objects will collide with
-	 */
-	private var foreground:FlxNapeTilemap;
-	
-	/**
-	 * The sprite that shadows will be drawn to
-	 */
-	private var shadowCanvas:FlxSprite;
-	
-	/**
-	 * The sprite that the actual darkness and the gem's flare-like effect will be drawn to
-	 */
-	private var shadowOverlay:FlxSprite;
-	
-	/**
-	 * The light source!
-	 */
-	private var gem:Gem;
+				
+	private var map:GameMap;	
 	
 	/**
 	 * If there's a small gap between something (could be two tiles,
@@ -79,37 +56,9 @@ class PlayState extends FlxState
 		
 		FlxNapeSpace.init();
 		FlxNapeSpace.space.gravity.setxy(0, 1200);
-		FlxNapeSpace.drawDebug = false; // You can toggle this on/off one by pressing 'D'
-		
-		var background:FlxTilemap = new FlxTilemap();
-		background.loadMapFromCSV("assets/data/background.txt",
-			"assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
-		add(background);
-		
-		// Note: The tilemap used in this demo was drawn with 'Tiled' (http://www.mapeditor.org/),
-		// but the level data was picked from the .tmx file and put into two separate
-		// .txt files for simplicity. If you wish to learn how to use Tiled with your project,
-		// have a look at this demo: http://haxeflixel.com/demos/TiledEditor/
-		
-		// If we add the shadows *before* all of the foreground elements (stage included)
-		// they will only cover the background, which is usually what you'd want I'd guess :)
-		shadowCanvas = new FlxSprite();
-		shadowCanvas.blend = BlendMode.MULTIPLY;
-		shadowCanvas.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT, true);
-		add(shadowCanvas);
-		
-		foreground = new FlxNapeTilemap();
-		foreground.loadMapFromCSV("assets/data/foreground.txt",
-			"assets/images/tiles.png", TILE_SIZE, TILE_SIZE, null, 1, 1);
-		add(foreground);
-		
-		foreground.setupTileIndices([4]);
-		createProps();
-		
-		shadowOverlay = new FlxSprite();
-		shadowOverlay.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT, true);
-		shadowOverlay.blend = BlendMode.MULTIPLY;
-		add(shadowOverlay);
+		FlxNapeSpace.drawDebug = false; // You can toggle this on/off one by pressing 'D'		
+			
+		map = new GameMap(this);
 		
 		infoText = new FlxText(10, 10, 100, "");
 		add(infoText);
@@ -118,41 +67,7 @@ class PlayState extends FlxState
 		fps = new FPS(10, 10, 0xffffff);
 		FlxG.stage.addChild(fps);
 		fps.visible = false;
-	}
-	
-	private function createProps():Void
-	{
-		for (tileY in 0...foreground.heightInTiles)
-		{
-			for (tileX in 0...foreground.widthInTiles)
-			{
-				var tileIndex = foreground.getTile(tileX, tileY);
-				var xPos:Float = tileX * TILE_SIZE;
-				var yPos:Float = tileY * TILE_SIZE;
-				
-				if (tileIndex == Prop.BARREL)
-				{					
-					add(new Barrel(xPos, yPos));
-					cleanTile(tileX, tileY);
-				}
-				else if (tileIndex == Prop.GEM)
-				{
-					gem = new Gem(xPos, yPos);
-					add(gem);
-					cleanTile(tileX, tileY);
-				}
-			}
-		}
-	}
-	
-	/**
-	 * The tile in question was replaced by an actual object, this function will clean up the tile for us
-	 */
-	private function cleanTile(x:Int, y:Int):Void
-	{
-		foreground.setTile(x, y, 0);
-	}
-	
+	}	
 	override public function update(elapsed:Float):Void
 	{
 		infoText.text = "FPS: " + fps.currentFPS + "\n\nObjects can be dragged/thrown around.\n\nPress 'R' to restart.";
@@ -169,17 +84,17 @@ class PlayState extends FlxState
 	
 	public function processShadows():Void
 	{
-		shadowCanvas.fill(FlxColor.TRANSPARENT);
-		shadowOverlay.fill(OVERLAY_COLOR);
+		map.shadowCanvas.fill(FlxColor.TRANSPARENT);
+		map.shadowOverlay.fill(OVERLAY_COLOR);
 
-		shadowOverlay.drawCircle( // outer red circle
-			gem.body.position.x + FlxG.random.float( -.6, .6),
-			gem.body.position.y + FlxG.random.float( -.6, .6),
+		map.shadowOverlay.drawCircle( // outer red circle
+			map.gem.body.position.x + FlxG.random.float( -.6, .6),
+			map.gem.body.position.y + FlxG.random.float( -.6, .6),
 			(FlxG.random.bool(5) ? 16 : 16.5), 0xffff5f5f);
 
-		shadowOverlay.drawCircle( // inner red circle
-			gem.body.position.x + FlxG.random.float( -.25, .25),
-			gem.body.position.y + FlxG.random.float( -.25, .25),
+		map.shadowOverlay.drawCircle( // inner red circle
+			map.gem.body.position.x + FlxG.random.float( -.25, .25),
+			map.gem.body.position.y + FlxG.random.float( -.25, .25),
 			(FlxG.random.bool(5) ? 13 : 13.5), 0xffff7070);
 
 		for (body in FlxNapeSpace.space.bodies)
@@ -207,8 +122,8 @@ class PlayState extends FlxState
 	private function processShapeVertex(startVertex:Vec2, endVertex:Vec2):Void
 	{
 		var tempLightOrigin:Vec2 = Vec2.get(
-			gem.body.position.x + FlxG.random.float( -.3, 3),
-			gem.body.position.y + FlxG.random.float(-.3, .3));
+			map.gem.body.position.x + FlxG.random.float( -.3, 3),
+			map.gem.body.position.y + FlxG.random.float(-.3, .3));
 		
 		if (doesEdgeCastShadow(startVertex, endVertex, tempLightOrigin))
 		{
@@ -221,7 +136,7 @@ class PlayState extends FlxState
 				FlxPoint.weak(endVertex.x, endVertex.y)
 			];
 			
-			shadowCanvas.drawPolygon(vts, SHADOW_COLOR, lineStyle);
+			map.shadowCanvas.drawPolygon(vts, SHADOW_COLOR, lineStyle);
 		}
 	}
 	
@@ -246,11 +161,4 @@ class PlayState extends FlxState
 	 
 		return normal.dot(lightToStart) > 0;
 	}
-}
-
-@:enum
-abstract Prop(Int) to Int
-{
-	var BARREL = 5;
-	var GEM = 6;
 }
