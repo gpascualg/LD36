d7b0c2e4ed28d7fd3d06a03d9ff7a0ae2210a4f2
4nc3str4l <murielmaths@gmail.com>
2016-08-27 09:41:56 +0200

    Skip splash

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,10 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	var skipSplash:Bool = true;
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(640, 320, PlayState));
+		addChild(new FlxGame(640, 320, PlayState, 1, 60, true, false));
 	}
 }
